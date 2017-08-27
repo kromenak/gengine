e21326c4ef8ccc24169e0067b33ef0a410b7f43a
Clark Kromenaker <kromenak@gmail.com>
2017-08-27 13:52:55 -0700

    Added a basic types header, which is meant to define a common set of data types of various sizes for use across the project.

---
+++ Source/Types.h
@@ -0,0 +1,19 @@
+//
+//  Types.h
+//  GEngine
+//
+//  Created by Clark Kromenaker on 8/17/17.
+//
+
+#pragma once
+
+#include <cstdint>
+
+typedef uint8_t uint8;
+typedef int8_t int8;
+
+typedef uint16_t uint16;
+typedef int16_t int16;
+
+typedef uint32_t uint32;
+typedef int32_t int32;
