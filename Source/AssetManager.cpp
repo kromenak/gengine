1261fb18015c7d5f1d1e806408ba2008193f19bf
Clark Kromenaker <kromenak@gmail.com>
2017-08-27 13:58:13 -0700

    Wrapped Barn file management within a more general Asset Manager class.
    The idea here is that we can request any runtime asset object from the asset manager. The asset manager will maintain a prioritized list of paths on the file system to search for the asset. If no asset is found, it will then search any Barn files that are currently loaded for the asset. Therefore, assets on the disk take priority over assets in barn files, which is consistent with the original GK3 game.

---
+++ Source/AssetManager.cpp
@@ -0,0 +1,123 @@
+//
+//  AssetManager.cpp
+//  GEngine
+//
+//  Created by Clark Kromenaker on 8/17/17.
+//
+
+#include "AssetManager.h"
+#include <iostream>
+
+AssetManager::AssetManager()
+{
+    
+}
+
+void AssetManager::AddSearchPath(string searchPath)
+{
+    // If the search path already exists in the list, don't add it again.
+    if(find(mSearchPaths.begin(), mSearchPaths.end(), searchPath) != mSearchPaths.end())
+    {
+        return;
+    }
+    mSearchPaths.push_back(searchPath);
+}
+
+void AssetManager::LoadBarn(string barnName)
+{
+    // If the barn is already in the map, then we don't need to load it again.
+    if(mLoadedBarns.find(barnName) != mLoadedBarns.end()) { return; }
+    
+    string assetPath = GetAssetPath(barnName);
+    if(assetPath.empty())
+    {
+        cout << "Barn doesn't exist at any search path." << endl;
+        return;
+    }
+    
+    BarnFile* barn = new BarnFile(assetPath.c_str());
+    mLoadedBarns[assetPath] = barn;
+}
+
+void AssetManager::UnloadBarn(string barnName)
+{
+    // If the barn isn't in the map, we can't unload it!
+    auto iter = mLoadedBarns.find(barnName);
+    if(iter == mLoadedBarns.end()) { return; }
+
+    //TODO: Do we need to unload all loaded assets from this barn?
+    
+    // Delete barn.
+    BarnFile* barn = iter->second;
+    delete barn;
+    
+    // Remove from map.
+    mLoadedBarns.erase(barnName);
+}
+
+Audio* AssetManager::LoadAudio(string audioName)
+{
+    // First, see if the asset exists at any asset search path.
+    // If so, we load the asset directly from file.
+    string assetPath = GetAssetPath(audioName);
+    if(!assetPath.empty())
+    {
+        //TODO: Load asset from file.
+        return nullptr;
+    }
+    
+    // If no file to load, we'll get the asset from a barn.
+    BarnFile* barn = GetContainingBarn(audioName);
+    if(barn != nullptr)
+    {
+        BarnAsset* asset = barn->GetAsset(audioName);
+        char* buffer = new char[asset->uncompressedSize];
+        barn->Extract(audioName, buffer, asset->uncompressedSize);
+        
+        Audio* audio = new Audio(audioName, buffer, asset->uncompressedSize);
+        return audio;
+    }
+    
+    // Couldn't find this asset!
+    return nullptr;
+}
+
+string AssetManager::GetAssetPath(string fileName)
+{
+    for(const string& searchPath : mSearchPaths)
+    {
+        string path = searchPath + fileName;
+        ifstream f(path.c_str());
+        if(f.good())
+        {
+            return path;
+        }
+    }
+    return "";
+}
+
+BarnFile* AssetManager::GetContainingBarn(string fileName)
+{
+    for(const auto& entry : mLoadedBarns)
+    {
+        BarnAsset* asset = entry.second->GetAsset(fileName);
+        if(asset != nullptr)
+        {
+            return entry.second;
+        }
+    }
+    return nullptr;
+}
+
+BarnAsset* AssetManager::GetBarnAsset(string fileName)
+{
+    for(const auto& entry : mLoadedBarns)
+    {
+        BarnAsset* asset = entry.second->GetAsset(fileName);
+        if(asset != nullptr)
+        {
+            return asset;
+        }
+    }
+    return nullptr;
+}
---
+++ Source/AssetManager.h
@@ -0,0 +1,43 @@
+//
+//  AssetManager.h
+//  GEngine
+//
+//  Created by Clark Kromenaker on 8/17/17.
+//
+
+#pragma once
+#include <string>
+#include <vector>
+#include "BarnFile.h"
+#include "Audio.h"
+
+using namespace std;
+
+class AssetManager
+{
+public:
+    
+private:
+    // A list of paths to search for assets.
+    // In priority order, since we'll search in order, and stop when we find the item.
+    vector<string> mSearchPaths;
+    
+    // A map of loaded barn files. If an asset isn't found on any search path,
+    // we then search each loaded barn file for the asset.
+    unordered_map<string, BarnFile*> mLoadedBarns;
+    
+public:
+    AssetManager();
+    
+    void AddSearchPath(string searchPath);
+    
+    void LoadBarn(string barnName);
+    void UnloadBarn(string barnName);
+    
+    Audio* LoadAudio(string audioName);
+    
+private:
+    string GetAssetPath(string fileName);
+    BarnFile* GetContainingBarn(string fileName);
+    BarnAsset* GetBarnAsset(string fileName);
+};
