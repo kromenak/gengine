8a4f306e770b75f8dc835ad685de6311ff3df6fa
Clark Kromenaker <kromenak@gmail.com>
2017-09-02 19:09:51 -0700

    Added copying of libmad to executable directory.

--- Xcode/CopyLibraries.sh
+++ Xcode/CopyLibraries.sh
@@ -3,9 +3,11 @@
 if [ -d "$BUILD_DIR/Debug" ]; then
 #cp "$SRCROOT"/../Libraries/GLEW/lib/mac/*.dylib $BUILD_DIR/Debug
 cp "$SRCROOT"/../Libraries/SDL/lib/mac/*.dylib $BUILD_DIR/Debug
+cp "$SRCROOT"/../Libraries/libmad/lib/mac/*.dylib $BUILD_DIR/Debug
 fi
 
 if [ -d "$BUILD_DIR/Release" ]; then
 #cp "$SRCROOT"/../Libraries/GLEW/lib/mac/*.dylib $BUILD_DIR/Release
 cp "$SRCROOT"/../Libraries/SDL/lib/mac/*.dylib $BUILD_DIR/Release
+cp "$SRCROOT"/../Libraries/libmad/lib/mac/*.dylib $BUILD_DIR/Release
 fi
