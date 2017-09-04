f254a71ee0cbce77f6cf733ef87cf80fd49038c0
Clark Kromenaker <kromenak@gmail.com>
2017-09-03 17:35:33 -0700

    Added GLEW library, which will be needed to use OpenGL extensions.

--- Xcode/CopyLibraries.sh
+++ Xcode/CopyLibraries.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 if [ -d "$BUILD_DIR/Debug" ]; then
-#cp "$SRCROOT"/../Libraries/GLEW/lib/mac/*.dylib $BUILD_DIR/Debug
+cp "$SRCROOT"/../Libraries/GLEW/lib/mac/*.dylib $BUILD_DIR/Debug
 cp "$SRCROOT"/../Libraries/SDL/lib/mac/*.dylib $BUILD_DIR/Debug
 cp "$SRCROOT"/../Libraries/libmad/lib/mac/*.dylib $BUILD_DIR/Debug
 cp "$SRCROOT"/../Libraries/zlib/lib/mac/*.dylib $BUILD_DIR/Debug
 fi
 
 if [ -d "$BUILD_DIR/Release" ]; then
-#cp "$SRCROOT"/../Libraries/GLEW/lib/mac/*.dylib $BUILD_DIR/Release
+cp "$SRCROOT"/../Libraries/GLEW/lib/mac/*.dylib $BUILD_DIR/Release
 cp "$SRCROOT"/../Libraries/SDL/lib/mac/*.dylib $BUILD_DIR/Release
 cp "$SRCROOT"/../Libraries/libmad/lib/mac/*.dylib $BUILD_DIR/Release
 cp "$SRCROOT"/../Libraries/zlib/lib/mac/*.dylib $BUILD_DIR/Release
