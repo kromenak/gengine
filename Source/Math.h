2df4092d3c43541c66f4ab37803aa05273b4a478
Clark Kromenaker <kromenak@gmail.com>
2017-12-23 15:33:27 -0600

    Added a couple more public functions for asin, acos, atan.

--- Source/Math.h
+++ Source/Math.h
@@ -11,7 +11,12 @@ namespace Math
 {
     // Dictates that floating-point numbers within 0.000001 units are equal to one another.
     static const float kEpsilon = 1.0e-6f;
-    //TODO: PI Stuff
+    
+    // Pi-related constants.
+    static const float kPi = 3.1415926535897932384626433832795f;
+    static const float k2Pi = 2.0f * kPi;
+    static const float kPiOver2 = kPi / 2.0f;
+    static const float kPiOver4 = kPi / 4.0f;
     
     // Calculates the square root of a value.
     inline float Sqrt(float val)
@@ -40,13 +45,28 @@ namespace Math
         return std::sinf(radians);
     }
     
+    inline float Asin(float ratio)
+    {
+        return std::asinf(ratio);
+    }
+    
     inline float Cos(float radians)
     {
         return std::cosf(radians);
     }
     
+    inline float Acos(float ratio)
+    {
+        return std::acosf(ratio);
+    }
+    
     inline float Tan(float radians)
     {
         return std::tanf(radians);
+    }
+    
+    inline float Atan(float ratio)
+    {
+        return std::atanf(ratio);
     }
 }
