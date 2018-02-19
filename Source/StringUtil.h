76e8856d025bc49c7aebf9fff18419cecf3baf99
Clark Kromenaker <kromenak@gmail.com>
2018-02-18 19:27:33 -0800

    Added "Trim" StringUtil method.

--- Source/StringUtil.h
+++ Source/StringUtil.h
@@ -15,4 +15,17 @@ namespace StringUtil
     {
         std::transform(str.begin(), str.end(), str.begin(), ::toupper);
     }
+    
+    inline void Trim(std::string& str)
+    {
+        // Find first non-space character index. If we can't (str is all spaces), just return.
+        size_t first = str.find_first_not_of(' ');
+        if(first == string::npos) { return; }
+        
+        // Find first non-space in the back.
+        size_t last = str.find_last_not_of(' ');
+        
+        // Finally, trim off the front and back whitespace.
+        str = str.substr(first, (last - first + 1));
+    }
 }
