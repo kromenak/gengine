5858192549f04e8974758eeb8ca491ad836f8f71
Clark Kromenaker <kromenak@gmail.com>
2018-02-20 00:44:02 -0800

    Added StringUtil methods for ToUpper/ToLower that make copies if needed. Also added a case-insensitive equals function.

--- Source/StringUtil.h
+++ Source/StringUtil.h
@@ -16,16 +16,47 @@ namespace StringUtil
         std::transform(str.begin(), str.end(), str.begin(), ::toupper);
     }
     
+    inline std::string ToUpperCopy(std::string str)
+    {
+        std::transform(str.begin(), str.end(), str.begin(), ::toupper);
+        return str;
+    }
+    
+    inline void ToLower(std::string& str)
+    {
+        std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+    }
+    
+    inline std::string ToLowerCopy(std::string str)
+    {
+        std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+        return str;
+    }
+    
     inline void Trim(std::string& str)
     {
         // Find first non-space character index. If we can't (str is all spaces), just return.
         size_t first = str.find_first_not_of(' ');
-        if(first == string::npos) { return; }
+        if(first == std::string::npos) { return; }
         
         // Find first non-space in the back.
         size_t last = str.find_last_not_of(' ');
         
         // Finally, trim off the front and back whitespace.
         str = str.substr(first, (last - first + 1));
+    }
+    
+    // Struct that encapsulates a case-insensitive character comparison.
+    struct iequal
+    {
+        bool operator()(int c1, int c2) const
+        {
+            return std::toupper(c1) == std::toupper(c2);
+        }
+    };
+    
+    inline bool EqualsIgnoreCase(const std::string& str1, const std::string& str2)
+    {
+        return std::equal(str1.begin(), str1.end(), str2.begin(), iequal());
     }
 }
