2221dbc75f584be462c8a1af8d31f0adb4fa5da8
Clark Kromenaker <kromenak@gmail.com>
2018-03-19 17:52:10 -0700

    Integrated SDL_mixer v2.0.0, which uses smpeg for MP3 playback. Tried v2.0.2, which uses mpg123, but it doesn't work! Internet says this is a known bug on OSX.

--- Xcode/CopyLibraries.sh
+++ Xcode/CopyLibraries.sh
@@ -5,7 +5,6 @@ cp "$SRCROOT"/../Libraries/GLEW/lib/mac/*.dylib $BUILD_DIR/Debug
 #cp "$SRCROOT"/../Libraries/SDL/lib/mac/*.dylib $BUILD_DIR/Debug
 cp -r "$SRCROOT"/../Libraries/SDL/SDL2.framework $BUILD_DIR/Debug/SDL2.framework
 cp -r "$SRCROOT"/../Libraries/SDL/SDL2_mixer.framework $BUILD_DIR/Debug/SDL2_mixer.framework
-cp "$SRCROOT"/../Libraries/libmad/lib/mac/*.dylib $BUILD_DIR/Debug
 cp "$SRCROOT"/../Libraries/zlib/lib/mac/*.dylib $BUILD_DIR/Debug
 
 cp -R "$SRCROOT"/../Assets $BUILD_DIR/Debug
@@ -16,7 +15,6 @@ cp "$SRCROOT"/../Libraries/GLEW/lib/mac/*.dylib $BUILD_DIR/Release
 #cp "$SRCROOT"/../Libraries/SDL/lib/mac/*.dylib $BUILD_DIR/Release
 cp -r "$SRCROOT"/../Libraries/SDL/SDL2.framework $BUILD_DIR/Release/SDL2.framework
 cp -r "$SRCROOT"/../Libraries/SDL/SDL2_mixer.framework $BUILD_DIR/Release/SDL2_mixer.framework
-cp "$SRCROOT"/../Libraries/libmad/lib/mac/*.dylib $BUILD_DIR/Release
 cp "$SRCROOT"/../Libraries/zlib/lib/mac/*.dylib $BUILD_DIR/Release
 
 cp -R "$SRCROOT"/../Assets $BUILD_DIR/Release
