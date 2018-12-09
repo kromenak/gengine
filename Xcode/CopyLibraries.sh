ac9c9220723704bfe6c9c80c1c4c78729a776998
Clark Kromenaker <kromenak@gmail.com>
2018-12-09 01:36:23 -0800

    Got rid of SDL loose include/lib files, since I'm using the framework. Updated CopyLibraries to no longer copy copy SDL_mixer and SDL loose files.

--- Xcode/CopyLibraries.sh
+++ Xcode/CopyLibraries.sh
@@ -2,9 +2,7 @@
 
 if [ -d "$BUILD_DIR/Debug" ]; then
 cp "$SRCROOT"/../Libraries/GLEW/lib/mac/*.dylib $BUILD_DIR/Debug
-#cp "$SRCROOT"/../Libraries/SDL/lib/mac/*.dylib $BUILD_DIR/Debug
 cp -r "$SRCROOT"/../Libraries/SDL/SDL2.framework $BUILD_DIR/Debug/SDL2.framework
-cp -r "$SRCROOT"/../Libraries/SDL/SDL2_mixer.framework $BUILD_DIR/Debug/SDL2_mixer.framework
 cp "$SRCROOT"/../Libraries/zlib/lib/mac/*.dylib $BUILD_DIR/Debug
 cp "$SRCROOT"/../Libraries/fmod/lib/*.dylib $BUILD_DIR/Debug
 
@@ -13,9 +11,7 @@ fi
 
 if [ -d "$BUILD_DIR/Release" ]; then
 cp "$SRCROOT"/../Libraries/GLEW/lib/mac/*.dylib $BUILD_DIR/Release
-#cp "$SRCROOT"/../Libraries/SDL/lib/mac/*.dylib $BUILD_DIR/Release
 cp -r "$SRCROOT"/../Libraries/SDL/SDL2.framework $BUILD_DIR/Release/SDL2.framework
-cp -r "$SRCROOT"/../Libraries/SDL/SDL2_mixer.framework $BUILD_DIR/Release/SDL2_mixer.framework
 cp "$SRCROOT"/../Libraries/zlib/lib/mac/*.dylib $BUILD_DIR/Release
 cp "$SRCROOT"/../Libraries/fmod/lib/*.dylib $BUILD_DIR/Release
 
