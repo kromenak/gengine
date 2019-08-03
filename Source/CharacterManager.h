a542424e70225f3ab376957db6a015df6931587d
Clark Kromenaker <kromenak@gmail.com>
2019-08-02 19:17:12 -0700

    CharacterManager now reads in all FACE.TXT parameters (except unneeded eye data) for all characters, including defaults.

--- Source/CharacterManager.h
+++ Source/CharacterManager.h
@@ -20,38 +20,65 @@ class Texture;
 
 struct FaceConfig
 {
+	// Default textures for face/eyelids/forehead.
+	Texture* faceTexture = nullptr;
+	Texture* eyelidsTexture = nullptr;
+	Texture* foreheadTexture = nullptr;
+	
+	// Left and right eye textures.
 	Texture* leftEyeTexture = nullptr;
 	Texture* rightEyeTexture = nullptr;
 	
+	// Offset (in pixels) of top-left corner of eyes in the face texture.
 	Vector2 leftEyeOffset;
 	Vector2 rightEyeOffset;
 	
+	// ???
 	Vector2 leftEyeBias;
 	Vector2 rightEyeBias;
 	
+	// Max distance left/right eyes can move, in pixels.
 	Vector2 maxEyeLookDistance;
 	
+	// Frequency of eye jitters in milliseconds.
+	// And max jitter distance in each direction, in subpixels.
 	Vector2 eyeJitterFrequency;
 	Vector2 maxEyeJitterDistance;
 	
+	// Horizontal & vertical FOV, in degrees.
+	// Second one is used to determine whether to move head.
 	Vector2 eyeFieldOfView;
 	Vector2 eyeShortFieldOfView;
+	
+	// How far apart eyes are, in inches.
 	float eyeSeparation = 0.0f;
 	
+	// Approximate head radius, in inches.
+	// And an offset to "virtual" head center, in inches (everyone just uses (0,0,0)).
 	float headRadius = 0.0f;
 	Vector3 headCenterOffset;
 	
+	// Offset (in pixels) of top-left corner of forehead in the face texture.
 	Vector2 foreheadOffset;
 	
+	// Offset (in pixels) of top-left corner of eyelids in the face texture.
 	Vector2 eyelidsOffset;
+	
+	// An optional(?) alpha channel for the eyelids.
+	// Seems to be used to determine where eye textures should show?
 	Texture* eyelidsAlphaChannel = nullptr;
 	
+	// Up to two possible blink anims with different probabilities.
 	Animation* blinkAnim1 = nullptr;
 	int blinkAnim1Probability = 0;
 	Animation* blinkAnim2 = nullptr;
 	int blinkAnim2Probability = 0;
+	
+	// Min and max period between blinks, in milliseconds.
 	Vector2 blinkFrequency;
 	
+	// Offset (in pixels) of top-left corner of mouth in the face texture.
+	// Plus, mouth size? But mouth textures all match this size anyway.
 	Vector2 mouthOffset;
 	Vector2 mouthSize;
 };
