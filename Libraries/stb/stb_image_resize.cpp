77e5436172a7947a6bdf41a8d1c6e163e47c0384
Clark Kromenaker <kromenak@gmail.com>
2019-08-10 10:45:07 -0700

    Added "image resize" portion of stb library (https://github.com/nothings/stb), which I'm using at the moment to downsample eye textures for face assembly.

---
+++ Libraries/stb/stb_image_resize.cpp
@@ -0,0 +1,8 @@
+// Implementation of stb_image_resize library
+
+// Per stb documentation, define this before including the header.
+// The implementation will appear in this file.
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+
+// Include the header to make it happen!
+#include "stb_image_resize.h"
