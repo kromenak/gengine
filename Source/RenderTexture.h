0686906a0b67f3562e72abd17ef897f0c4b35621
Clark Kromenaker <kromenak@gmail.com>
2019-08-10 10:43:39 -0700

    Added a RenderTexture class to experiment with render-to-texture techniques for actor faces. However, this proved to be quite complicated and didn't feel like a good solution for assembling the face textures. Gonna keep the class, though, in case we want to do something with it in the future.

---
+++ Source/RenderTexture.h
@@ -0,0 +1,34 @@
+//
+// RenderTexture.h
+//
+// Clark Kromenaker
+//
+// A texture that can be rendered to. When activated, render commands
+// will go to the texture instead of the screen.
+//
+// After rendering to the texture, the texture can be used as an
+// input to other rendering operations that ultimately go to the screen.
+//
+// In OpenGL, this is implemented using "Framebuffer Objects."
+//
+#pragma once
+#include <GL/glew.h>
+
+#include "Texture.h"
+
+class RenderTexture
+{
+public:
+	RenderTexture(int width, int height);
+	~RenderTexture();
+	
+	void Activate();
+	
+	Texture& GetTexture() { return mRenderTexture; }
+	
+private:
+	GLuint mFboId = GL_NONE;
+	Texture mRenderTexture;
+	
+	GLuint mRenderTextureId = GL_NONE;
+};
