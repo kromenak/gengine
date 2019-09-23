14bceace50ee9161ce98e6710c1085947891aafa
Clark Kromenaker <kromenak@usc.edu>
2019-09-22 17:10:51 -0700

    Keyboard inputs for game camera no longer work if a text input field is capturing keyboard input.

--- Source/GameCamera.cpp
+++ Source/GameCamera.cpp
@@ -22,53 +22,64 @@ GameCamera::GameCamera()
 
 void GameCamera::UpdateInternal(float deltaTime)
 {
-    // Determine camera speed.
-    float camSpeed = kCameraSpeed;
-    if(Services::GetInput()->IsKeyPressed(SDL_SCANCODE_LSHIFT))
-    {
-        camSpeed = kCameraSpeed * kRunCameraMultiplier;
-    }
-    
-    // Forward and backward movement.
-    if(Services::GetInput()->IsKeyPressed(SDL_SCANCODE_W))
-    {
-        GetTransform()->Translate(GetForward() * (camSpeed * deltaTime));
-    }
-    else if(Services::GetInput()->IsKeyPressed(SDL_SCANCODE_S))
-    {
-        GetTransform()->Translate(GetForward() * (-camSpeed * deltaTime));
-    }
-    
-    // Up and down movement.
-    if(Services::GetInput()->IsKeyPressed(SDL_SCANCODE_E))
-    {
-        GetTransform()->Translate(Vector3(0.0f, camSpeed * deltaTime, 0.0f));
-    }
-    else if(Services::GetInput()->IsKeyPressed(SDL_SCANCODE_Q))
-    {
-        GetTransform()->Translate(Vector3(0.0f, -camSpeed * deltaTime, 0.0f));
-    }
-    
-    // Rotate left and right movement.
-    if(Services::GetInput()->IsKeyPressed(SDL_SCANCODE_A))
-    {
-        GetTransform()->Rotate(Vector3::UnitY, -kCameraRotationSpeed * deltaTime);
-    }
-    else if(Services::GetInput()->IsKeyPressed(SDL_SCANCODE_D))
-    {
-        GetTransform()->Rotate(Vector3::UnitY, kCameraRotationSpeed * deltaTime);
-    }
-    
-    // Rotate up and down movement.
-    if(Services::GetInput()->IsKeyPressed(SDL_SCANCODE_C))
-    {
-        GetTransform()->Rotate(GetRight(), -kCameraRotationSpeed * deltaTime);
-    }
-    else if(Services::GetInput()->IsKeyPressed(SDL_SCANCODE_Z))
-    {
-        GetTransform()->Rotate(GetRight(), kCameraRotationSpeed * deltaTime);
-    }
+	// If someone is capturing text input, key inputs should not affect the game.
+	if(!Services::GetInput()->IsTextInput())
+	{
+		// Determine camera speed.
+		float camSpeed = kCameraSpeed;
+		if(Services::GetInput()->IsKeyPressed(SDL_SCANCODE_LSHIFT))
+		{
+			camSpeed = kCameraSpeed * kRunCameraMultiplier;
+		}
+		
+		// Forward and backward movement.
+		if(Services::GetInput()->IsKeyPressed(SDL_SCANCODE_W))
+		{
+			GetTransform()->Translate(GetForward() * (camSpeed * deltaTime));
+		}
+		else if(Services::GetInput()->IsKeyPressed(SDL_SCANCODE_S))
+		{
+			GetTransform()->Translate(GetForward() * (-camSpeed * deltaTime));
+		}
+		
+		// Up and down movement.
+		if(Services::GetInput()->IsKeyPressed(SDL_SCANCODE_E))
+		{
+			GetTransform()->Translate(Vector3(0.0f, camSpeed * deltaTime, 0.0f));
+		}
+		else if(Services::GetInput()->IsKeyPressed(SDL_SCANCODE_Q))
+		{
+			GetTransform()->Translate(Vector3(0.0f, -camSpeed * deltaTime, 0.0f));
+		}
+		
+		// Rotate left and right movement.
+		if(Services::GetInput()->IsKeyPressed(SDL_SCANCODE_A))
+		{
+			GetTransform()->Rotate(Vector3::UnitY, -kCameraRotationSpeed * deltaTime);
+		}
+		else if(Services::GetInput()->IsKeyPressed(SDL_SCANCODE_D))
+		{
+			GetTransform()->Rotate(Vector3::UnitY, kCameraRotationSpeed * deltaTime);
+		}
+		
+		// Rotate up and down movement.
+		if(Services::GetInput()->IsKeyPressed(SDL_SCANCODE_C))
+		{
+			GetTransform()->Rotate(GetRight(), -kCameraRotationSpeed * deltaTime);
+		}
+		else if(Services::GetInput()->IsKeyPressed(SDL_SCANCODE_Z))
+		{
+			GetTransform()->Rotate(GetRight(), kCameraRotationSpeed * deltaTime);
+		}
+		
+		// For debugging, output camera position on key press.
+		if(Services::GetInput()->IsKeyDown(SDL_SCANCODE_P))
+		{
+			std::cout << GetPosition() << std::endl;
+		}
+	}
 	
+	// Handle hovering and clicking on scene objects.
 	if(mCamera != nullptr)
 	{
 		// Calculate mouse click ray.
@@ -95,11 +106,5 @@ void GameCamera::UpdateInternal(float deltaTime)
 		{
 			GEngine::inst->GetScene()->Interact(ray);
 		}
-	}
-	
-	// For debugging, output camera position.
-	if(Services::GetInput()->IsKeyDown(SDL_SCANCODE_P))
-	{
-		std::cout << GetPosition() << std::endl;
 	}
 }
