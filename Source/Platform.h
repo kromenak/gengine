8aff6b8a0cb1a6393a59633e8879218c6bb62b98
Clark Kromenaker <kromenak@usc.edu>
2019-10-13 19:57:41 -0700

    Added Platform header, which will provide a uniform way to identify what platform the game is running on.

---
+++ Source/Platform.h
@@ -0,0 +1,22 @@
+//
+// Platform.h
+//
+// Clark Kromenaker
+//
+// Helper for platform-abstraction. Provides a uniform/simplified way
+// to detect what platform we're running on.
+//
+#pragma once
+
+#if defined(__APPLE__)
+	#include "TargetConditionals.h"
+	#if defined(TARGET_OS_OSX)
+		#define PLATFORM_MAC
+	#else
+		#error "Unknown Apple Platform"
+	#endif
+#elif defined(_WIN32)
+	#define PLATFORM_WINDOWS
+#else
+	#error "Unknown Platform"
+#endif
