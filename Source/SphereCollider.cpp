5e9b3564e4ac106ecd91479563713bb467eeba52
Clark Kromenaker <kromenak@gmail.com>
2020-04-05 15:05:13 -0700

    Added SphereCollider component husk. It literally does nothing yet!

---
+++ Source/SphereCollider.cpp
@@ -0,0 +1,18 @@
+//
+// SphereCollider.cpp
+//
+// Clark Kromenaker
+//
+#include "SphereCollider.h"
+
+TYPE_DEF_CHILD(Component, SphereCollider);
+
+SphereCollider::SphereCollider(Actor* owner) : Component(owner)
+{
+	
+}
+
+void SphereCollider::OnUpdate(float deltaTime)
+{
+	
+}
