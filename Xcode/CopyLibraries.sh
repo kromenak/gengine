47b36e82eff1d2f369a637068e08f81e4358635f
Clark Kromenaker <kromenak@gmail.com>
2020-11-06 18:39:51 -0800

    Fiddled with settings to get macOS "app" version up and running again. Added a placeholder app icon for fun! Updated "copy libraries" script to correctly copy over assets and libraries for both .app and "command line" versions of app. No longer need both files.

--- Xcode/CopyLibraries.sh
+++ Xcode/CopyLibraries.sh
@@ -1,13 +1,28 @@
 #!/bin/sh
 
-cp -r "$SRCROOT"/../Assets $TARGET_BUILD_DIR
+# Copy to exe directory by default.
+ASSETS_DIR=$TARGET_BUILD_DIR
+LIBS_DIR=$TARGET_BUILD_DIR
 
-cp    "$SRCROOT"/../Libraries/ffmpeg/lib/mac/libavcodec.dylib $TARGET_BUILD_DIR
-cp    "$SRCROOT"/../Libraries/ffmpeg/lib/mac/libavformat.dylib $TARGET_BUILD_DIR
-cp    "$SRCROOT"/../Libraries/ffmpeg/lib/mac/libavutil.dylib $TARGET_BUILD_DIR
-cp    "$SRCROOT"/../Libraries/ffmpeg/lib/mac/libswresample.dylib $TARGET_BUILD_DIR
-cp    "$SRCROOT"/../Libraries/ffmpeg/lib/mac/libswscale.dylib $TARGET_BUILD_DIR
-cp    "$SRCROOT"/../Libraries/fmod/lib/libfmod.dylib $TARGET_BUILD_DIR
-cp    "$SRCROOT"/../Libraries/GLEW/lib/mac/libGLEW.dylib $TARGET_BUILD_DIR
-cp -r "$SRCROOT"/../Libraries/SDL/SDL2.framework $TARGET_BUILD_DIR
-cp    "$SRCROOT"/../Libraries/zlib/lib/mac/libz.dylib $TARGET_BUILD_DIR
+# If building mac app, directories are a bit different.
+if [ $1 = "app" ]; then
+	ASSETS_DIR=$TARGET_BUILD_DIR/$FULL_PRODUCT_NAME/Contents/Resources
+	LIBS_DIR=$TARGET_BUILD_DIR/$FULL_PRODUCT_NAME/Contents/Libraries
+	mkdir $LIBS_DIR
+fi
+
+# Copy assets folder
+cp -r "$SRCROOT"/../Assets $ASSETS_DIR
+
+# Copy libraries
+cp    "$SRCROOT"/../Libraries/ffmpeg/lib/mac/libavcodec.dylib $LIBS_DIR
+cp    "$SRCROOT"/../Libraries/ffmpeg/lib/mac/libavformat.dylib $LIBS_DIR
+cp    "$SRCROOT"/../Libraries/ffmpeg/lib/mac/libavutil.dylib $LIBS_DIR
+cp    "$SRCROOT"/../Libraries/ffmpeg/lib/mac/libswresample.dylib $LIBS_DIR
+cp    "$SRCROOT"/../Libraries/ffmpeg/lib/mac/libswscale.dylib $LIBS_DIR
+cp    "$SRCROOT"/../Libraries/fmod/lib/mac/libfmod.dylib $LIBS_DIR
+cp    "$SRCROOT"/../Libraries/GLEW/lib/mac/libGLEW.dylib $LIBS_DIR
+cp    "$SRCROOT"/../Libraries/zlib/lib/mac/libz.dylib $LIBS_DIR
+
+# Copy frameworks (not needed - Xcode does this for us)
+#cp -r "$SRCROOT"/../Libraries/SDL/SDL2.framework $TARGET_BUILD_DIR
--- Xcode/CopyLibrariesToApp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mkdir $TARGET_BUILD_DIR/$FULL_PRODUCT_NAME/Contents/Libs
-cp "$SRCROOT"/../Libraries/GLEW/lib/mac/*.dylib $TARGET_BUILD_DIR/$FULL_PRODUCT_NAME/Contents/Libs
-cp "$SRCROOT"/../Libraries/zlib/lib/mac/*.dylib $TARGET_BUILD_DIR/$FULL_PRODUCT_NAME/Contents/Libs
-cp "$SRCROOT"/../Libraries/fmod/lib/*.dylib $TARGET_BUILD_DIR/$FULL_PRODUCT_NAME/Contents/Libs
