8df3c962c7988f8a4e61115124baf3128044b148
Clark Kromenaker <kromenak@gmail.com>
2020-11-21 15:53:34 -0800

    Asset uses the new File function to get name without extension.

--- Source/Asset.cpp
+++ Source/Asset.cpp
@@ -5,6 +5,8 @@
 //
 #include "Asset.h"
 
+#include "FileSystem.h"
+
 Asset::Asset(std::string name) : mName(name)
 {
     
@@ -12,13 +14,5 @@ Asset::Asset(std::string name) : mName(name)
 
 std::string Asset::GetNameNoExtension()
 {
-    // Find '.' closest to end of name and get substring without point and extension.
-    for(int i = (int)mName.size() - 1; i >= 0; i--)
-    {
-        if(mName[i] == '.')
-        {
-            return mName.substr(0, i);
-        }
-    }
-    return mName;
+    return File::RemoveExtension(mName);
 }
