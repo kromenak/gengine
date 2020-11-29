cf8f6c087abc221853c9dd2d9e1fd6303c60265e
Clark Kromenaker <kromenak@gmail.com>
2020-11-29 15:09:38 -0800

    Added Windows-side deploy/install script. Using the custom "deploy" target in VS, the built project is copied to the "Bin" folder with all dependencies, copyrighted material is removed, and the project is zipped up, ready to be released.

---
+++ Scripts/Install.bat
@@ -0,0 +1,21 @@
+setlocal
+set BUILD_DIR=%1
+set INSTALL_DIR=%2
+
+rem Copy exe. Note the "echo d" tells xcopy that INSTALL_DIR is a directory and not a file.
+echo d | xcopy /y %BUILD_DIR%\gk3.exe %INSTALL_DIR%
+
+rem Copy all DLLs.
+xcopy /y %BUILD_DIR%\*.dll %INSTALL_DIR%
+
+rem Copy assets folder.
+xcopy /y /s /e /d  %BUILD_DIR%\Assets %INSTALL_DIR%\Assets\
+
+rem Delete any copyrighted materials.
+del %INSTALL_DIR%\Assets\GK3\*.brn
+del %INSTALL_DIR%\Assets\GK3\*.bik
+del %INSTALL_DIR%\Assets\GK3\*.avi
+
+rem Create zip file.
+powershell Compress-Archive -Path '%INSTALL_DIR%' -DestinationPath '%INSTALL_DIR%\GK3.zip' -Force
+endlocal
