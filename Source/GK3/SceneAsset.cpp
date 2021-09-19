3147d690862670c133d1f9bfd467898251331ec8
Clark Kromenaker <kromenak@gmail.com>
2021-09-19 16:59:06 -0700

    Experimented with loading light data from SCN assets. However, after some testing in the original game, I found that this data is not used to derive runtime lighting data. That being said, keeping the commented out parsing code just in case.

--- Source/GK3/SceneAsset.cpp
+++ Source/GK3/SceneAsset.cpp
@@ -1,8 +1,3 @@
-//
-// SceneAsset.cpp
-//
-// Clark Kromenaker
-//
 #include "SceneAsset.h"
 
 #include <iostream>
@@ -25,67 +20,146 @@ SceneAsset::~SceneAsset()
 void SceneAsset::ParseFromData(char *data, int dataLength)
 {
     IniParser parser(data, dataLength);
-    parser.ParseAll();
-    
-    // Load any BSP override specified. Otherwise, the name of the SCN asset is used for the BSP too.
-    std::vector<IniSection> topSections = parser.GetSections("");
-    for(auto& section : topSections)
+    parser.SetMultipleKeyValuePairsPerLine(false);
+
+    IniSection section;
+    while(parser.ReadNextSection(section))
     {
-        for(auto& line : section.lines)
+        if(section.name.empty())
         {
-			IniKeyValue& entry = line.entries.front();
-            if(StringUtil::EqualsIgnoreCase(entry.key, "bsp"))
+            for(auto& line : section.lines)
             {
-                mBspName = entry.value;
+                IniKeyValue& entry = line.entries.front();
+                if(StringUtil::EqualsIgnoreCase(entry.key, "bsp"))
+                {
+                    mBspName = entry.value;
+                }
             }
         }
+        else if(StringUtil::EqualsIgnoreCase(section.name, "Skybox"))
+        {
+            if(section.lines.size() > 0)
+            {
+                mSkybox = new Skybox();
+            }
+            for(auto& line : section.lines)
+            {
+                IniKeyValue& entry = line.entries.front();
+                if(StringUtil::EqualsIgnoreCase(entry.key, "left"))
+                {
+                    Texture* texture = Services::GetAssets()->LoadSceneTexture(entry.value);
+                    mSkybox->SetLeftTexture(texture);
+                }
+                else if(StringUtil::EqualsIgnoreCase(entry.key, "right"))
+                {
+                    Texture* texture = Services::GetAssets()->LoadSceneTexture(entry.value);
+                    mSkybox->SetRightTexture(texture);
+                }
+                else if(StringUtil::EqualsIgnoreCase(entry.key, "front"))
+                {
+                    Texture* texture = Services::GetAssets()->LoadSceneTexture(entry.value);
+                    mSkybox->SetFrontTexture(texture);
+                }
+                else if(StringUtil::EqualsIgnoreCase(entry.key, "back"))
+                {
+                    Texture* texture = Services::GetAssets()->LoadSceneTexture(entry.value);
+                    mSkybox->SetBackTexture(texture);
+                }
+                else if(StringUtil::EqualsIgnoreCase(entry.key, "up"))
+                {
+                    Texture* texture = Services::GetAssets()->LoadSceneTexture(entry.value);
+                    mSkybox->SetUpTexture(texture);
+                }
+                else if(StringUtil::EqualsIgnoreCase(entry.key, "down"))
+                {
+                    Texture* texture = Services::GetAssets()->LoadSceneTexture(entry.value);
+                    mSkybox->SetDownTexture(texture);
+                }
+            }
+        }
+        else if(StringUtil::EqualsIgnoreCase(section.name, "Lights") || StringUtil::EqualsIgnoreCase(section.name, "Models"))
+        {
+            // We are not doing anything with these sections right now - except ignoring them.
+        }
+        else
+        {
+            /*
+            // Any other section MUST BE (as far as I know) a light definition.
+            mLights.emplace_back();
+            mLights.back().name = section.name;
+
+            for(auto& line : section.lines)
+            {
+                IniKeyValue& entry = line.entries.front();
+                if(StringUtil::EqualsIgnoreCase(entry.key, "Type"))
+                {
+                    mLights.back().type = entry.GetValueAsInt();
+                }
+                else if(StringUtil::EqualsIgnoreCase(entry.key, "Position"))
+                {
+                    mLights.back().position = entry.GetValueAsVector3();
+                }
+                else if(StringUtil::EqualsIgnoreCase(entry.key, "Direction"))
+                {
+                    mLights.back().direction = entry.GetValueAsVector3();
+                }
+                else if(StringUtil::EqualsIgnoreCase(entry.key, "Color"))
+                {
+                    // Colors in SCN are floating point.
+                    // Since we don't yet have support for that, just read in as floats and convert to Color32.
+                    Vector3 color = entry.GetValueAsVector3();
+                    mLights.back().color = Color32(static_cast<int>(color.x * 255),
+                                                   static_cast<int>(color.y * 255),
+                                                   static_cast<int>(color.z * 255));
+                }
+                else if(StringUtil::EqualsIgnoreCase(entry.key, "Hotspot"))
+                {
+                    mLights.back().hotspot = entry.GetValueAsFloat();
+                }
+                else if(StringUtil::EqualsIgnoreCase(entry.key, "Falloff"))
+                {
+                    mLights.back().falloff = entry.GetValueAsFloat();
+                }
+                else if(StringUtil::EqualsIgnoreCase(entry.key, "AttenStart"))
+                {
+                    mLights.back().attenuationStart = entry.GetValueAsFloat();
+                }
+                else if(StringUtil::EqualsIgnoreCase(entry.key, "AttenEnd"))
+                {
+                    mLights.back().attenuationEnd = entry.GetValueAsFloat();
+                }
+                else if(StringUtil::EqualsIgnoreCase(entry.key, "UseAtten"))
+                {
+                    mLights.back().useAttenuation = entry.GetValueAsInt() != 0;
+                }
+                else if(StringUtil::EqualsIgnoreCase(entry.key, "CastShadows"))
+                {
+                    mLights.back().castShadows = entry.GetValueAsInt() != 0;
+                }
+                else if(StringUtil::EqualsIgnoreCase(entry.key, "Overshoot"))
+                {
+                    mLights.back().overshoot = entry.GetValueAsInt() != 0;
+                }
+                else if(StringUtil::EqualsIgnoreCase(entry.key, "Intensity"))
+                {
+                    mLights.back().intensity = entry.GetValueAsFloat();
+                }
+                else if(StringUtil::EqualsIgnoreCase(entry.key, "Radius"))
+                {
+                    mLights.back().radius = entry.GetValueAsFloat();
+                }
+                else if(StringUtil::EqualsIgnoreCase(entry.key, "DecayType"))
+                {
+                    mLights.back().decayType = entry.GetValueAsInt();
+                }
+            }
+            */
+        }
     }
-	
-	// If no BSP name was specified in the SCN file, default to the asset name with no extension.
-	if(mBspName.empty())
-	{
-		mBspName = GetNameNoExtension();
-	}
-    
-    // Load a skybox, if any.
-    IniSection skyboxSection = parser.GetSection("SKYBOX");
-    if(skyboxSection.lines.size() > 0)
+
+    // If no BSP name was specified in the SCN file, default to the asset name with no extension.
+    if(mBspName.empty())
     {
-        mSkybox = new Skybox();
-    }
-	for(auto& line : skyboxSection.lines)
-    {
-		IniKeyValue& entry = line.entries.front();
-        
-        if(StringUtil::EqualsIgnoreCase(entry.key, "left"))
-        {
-			Texture* texture = Services::GetAssets()->LoadSceneTexture(entry.value);
-            mSkybox->SetLeftTexture(texture);
-        }
-        else if(StringUtil::EqualsIgnoreCase(entry.key, "right"))
-        {
-			Texture* texture = Services::GetAssets()->LoadSceneTexture(entry.value);
-            mSkybox->SetRightTexture(texture);
-        }
-        else if(StringUtil::EqualsIgnoreCase(entry.key, "front"))
-        {
-			Texture* texture = Services::GetAssets()->LoadSceneTexture(entry.value);
-            mSkybox->SetFrontTexture(texture);
-        }
-        else if(StringUtil::EqualsIgnoreCase(entry.key, "back"))
-        {
-			Texture* texture = Services::GetAssets()->LoadSceneTexture(entry.value);
-            mSkybox->SetBackTexture(texture);
-        }
-        else if(StringUtil::EqualsIgnoreCase(entry.key, "up"))
-        {
-			Texture* texture = Services::GetAssets()->LoadSceneTexture(entry.value);
-            mSkybox->SetUpTexture(texture);
-        }
-        else if(StringUtil::EqualsIgnoreCase(entry.key, "down"))
-        {
-			Texture* texture = Services::GetAssets()->LoadSceneTexture(entry.value);
-            mSkybox->SetDownTexture(texture);
-        }
+        mBspName = GetNameNoExtension();
     }
 }
--- Source/GK3/SceneAsset.h
+++ Source/GK3/SceneAsset.h
@@ -11,7 +11,37 @@
 #pragma once
 #include "Asset.h"
 
+#include <string>
+#include <vector>
+
+#include "Color32.h"
+#include "Vector3.h"
+
 class Skybox;
+
+/*
+// SCN assets often have fairly complex lighting definitions in them.
+// However, after experimenting with the original game, as far as I can tell, they are not used at all for lighting at runtime.
+// Perhaps they were used for lightmap generation?
+struct SceneLight
+{
+    std::string name;
+    int type = 0;
+    Vector3 position;
+    Vector3 direction;
+    Color32 color;
+    float hotspot = 0.0f;
+    float falloff = 0.0f;
+    float attenuationStart = 0.0f;
+    float attenuationEnd = 0.0f;
+    bool useAttenuation = false;
+    bool castShadows = false;
+    bool overshoot = false;
+    float intensity = 1.0f;
+    float radius = 1.0f;
+    int decayType = 0;
+};
+*/
 
 class SceneAsset : public Asset
 {
@@ -28,6 +58,9 @@ private:
     
     // A skybox to use for the scene. This might also be specified in the SIF.
     Skybox* mSkybox = nullptr;
+
+    // Lights defined for this scene.
+    //std::vector<SceneLight> mLights;
     
     void ParseFromData(char* data, int dataLength);
 };
