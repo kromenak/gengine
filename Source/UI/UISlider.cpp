b1b3afe01fa2991d93e8bdfa0f85c9f13da27374
Clark Kromenaker <kromenak@gmail.com>
2021-12-29 17:52:56 -0800

    Added a Slider UI component, which is used to pick a value using a handle in a slideable area. This uses the drag component internally as the handle that you drag around to select a value.

---
+++ Source/UI/UISlider.cpp
@@ -0,0 +1,122 @@
+#include "UISlider.h"
+
+#include "Actor.h"
+#include "UIDrag.h"
+#include "Vector2.h"
+
+TYPE_DEF_CHILD(UIWidget, UISlider);
+
+UISlider::UISlider(Actor* owner) : UIWidget(owner)
+{
+    SetReceivesInput(true);
+}
+
+void UISlider::Render()
+{
+    // Anything?
+}
+
+void UISlider::SetHandleActor(Actor* handleActor)
+{
+    // Grab drag component.
+    mHandle = handleActor->GetComponent<UIDrag>();
+
+    //TODO: It'd be nice to do something like this, BUT...how to add it to the canvas?
+    //TODO: Currently, widgets don't know about their canvases, and they aren't added automatically.
+    //TODO: So, improving that flow/process is probably a good idea.
+    /*
+    if(mHandle == nullptr)
+    {
+        mHandle = mHandleActor->AddComponent<UIDrag>();
+    }
+    */
+
+    // We'll require that the handle is a child of the slider itself.
+    mHandle->GetRectTransform()->SetParent(GetRectTransform());
+
+    // We're going to assume for now: this is a horizontal slider with min value on left, max value on right.
+    // Set handle anchor/pivot so that at anchored position (0, 0) it'll be at value 0.
+    mHandle->GetRectTransform()->SetAnchor(0.0f, 0.5f);
+    mHandle->GetRectTransform()->SetPivot(0.0f, 0.5f);
+    mHandle->GetRectTransform()->SetAnchoredPosition(0.0f, 0.0f);
+}
+
+void UISlider::SetValue(float value)
+{
+    // Save old value, to see if it changed.
+    float oldValue = mValue;
+
+    // Value must be 0 to 1.   
+    mValue = Math::Clamp(value, 0.0f, 1.0f);
+
+    // Issue callback if value changed.
+    if(!Math::AreEqual(mValue, oldValue) && mValueChangeCallback != nullptr)
+    {
+        mValueChangeCallback(mValue);
+    }
+}
+
+void UISlider::SetValueSilently(float value)
+{
+    // Value must be 0 to 1.   
+    mValue = Math::Clamp(value, 0.0f, 1.0f);
+}
+
+float UISlider::GetValue() const
+{
+    return mValue;
+}
+
+void UISlider::OnUpdate(float deltaTime)
+{
+    if(mHandle != nullptr)
+    {
+        // Confine drag within the slider area.
+        // Need to do this in OnUpdate in case our RectTransform moves around.
+        mHandle->SetBoundaryRect(GetRectTransform()->GetWorldRect());
+
+        // If handle is being dragged, the value is driven by slider position.
+        // If handle is not dragged, slider position is driven by value.
+        if(mHandle->IsDragging())
+        {
+            SetValueFromSlider();
+        }
+        else
+        {
+            SetSliderFromValue();
+        }
+    }
+}
+
+void UISlider::SetSliderFromValue()
+{
+    if(mHandle != nullptr)
+    {
+        // Determine width of movement area for slider.
+        // This'll be width of slider, MINUS width of handle.
+        float width = GetRectTransform()->GetSize().x;
+        float handleWidth = mHandle->GetRectTransform()->GetSize().x;
+        float actualWidth = width - handleWidth;
+
+        // Calculate new handle pos.
+        Vector2 handlePos = mHandle->GetRectTransform()->GetAnchoredPosition();
+        handlePos.x = mValue * actualWidth;
+        mHandle->GetRectTransform()->SetAnchoredPosition(handlePos);
+    }
+}
+
+void UISlider::SetValueFromSlider()
+{
+    if(mHandle != nullptr)
+    {
+        // As above, actual move area is slider width MINUS handle width.
+        float width = GetRectTransform()->GetSize().x;
+        float handleWidth = mHandle->GetRectTransform()->GetSize().x;
+        float actualWidth = width - handleWidth;
+
+        // The handle pos is from the middle-left side of the handle, since we set the pivot to (0.0, 0.5).
+        // Therefore, the "max" position of the handle is (width - handleWidth). We need to take this into account for the final equation.
+        float handlePos = mHandle->GetRectTransform()->GetAnchoredPosition().x;
+        SetValue(handlePos / actualWidth);
+    }
+}
---
+++ Source/UI/UISlider.h
@@ -0,0 +1,45 @@
+//
+// Clark Kromenaker
+//
+// A UI element that can be clicked and dragged to change a numeric value.
+//
+#pragma once
+#include "UIWidget.h"
+
+#include <functional>
+
+class Texture;
+class UIDrag;
+
+class UISlider : public UIWidget
+{
+    TYPE_DECL_CHILD();
+public:
+    UISlider(Actor* owner);
+
+    void Render() override;
+
+    void SetHandleActor(Actor* handle);
+
+    void SetValueChangeCallback(std::function<void(float)> callback) { mValueChangeCallback = callback; }
+
+    void SetValue(float value);
+    void SetValueSilently(float value);
+    float GetValue() const;
+
+protected:
+    void OnUpdate(float deltaTime) override;
+
+private:
+    // Draggable object that represents the handle.
+    UIDrag* mHandle = nullptr;
+
+    // The *normalized* value of the slider.
+    float mValue = 1.0f;
+
+    // Called when the value of the slider changes.
+    std::function<void(float)> mValueChangeCallback;
+
+    void SetSliderFromValue();
+    void SetValueFromSlider();
+};
