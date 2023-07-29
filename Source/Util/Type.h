e13424a1bc3169b95fde1b45c72bd9d0549e5377
Clark Kromenaker <kromenak@gmail.com>
2023-07-29 14:31:14 -0700

    Added a Type macro for a class that is not meant to be inherited from. (Ended up not using this for anything yet, but useful to avoid compiler warnings in the future perhaps).

--- Source/Util/Type.h
+++ Source/Util/Type.h
@@ -1,16 +1,13 @@
 //
-// Type.h
-//
 // Clark Kromenaker
 //
-// Defines a "Type" type and macros for adding runtime type info (RTTI) to a
-// base class and subclasses.
+// Defines a "Type" and macros for adding runtime type info (RTTI) to a base class and subclasses.
 //
 #include <cstddef>
 #include <functional>   // for std::hash support
 #include <string>
 
-// A class's type will just be a size_t alias right now.
+// A class's type will just be a size_t alias for now.
 typedef std::size_t Type;
 
 // Converts passed in symbol to a string (MyClass => "MyClass").
@@ -21,9 +18,29 @@ typedef std::size_t Type;
 // Note: std::hash will not necessarily return the same value across runs of a program!
 #define GENERATE_TYPE(x) std::hash<std::string>()(SYMBOL_TO_STR(x));
 
-// Add inside of a base class declaration to add type info to that base class.
-// First class in class hierarchy that needs type info must specify this class.
-#define TYPE_DECL_BASE()                                              \
+// USAGE: Add TYPE_DECL* macro inside the class declaration.
+// USAGE: Add TYPE_DEF* macro inside the class implementation file.
+
+//////////////////////////////////////////////
+// Use this version for classes that DO NOT have children.
+#define TYPE_DECL()                                                 \
+private:                                                            \
+    static const Type type;                                         \
+    static const char* typeName;                                    \
+                                                                    \
+public:                                                             \
+    static Type GetType() { return type; }                          \
+    static const char* GetTypeName() { return typeName; }           \
+    bool IsTypeOf(const Type t) const { return t == type; }         \
+
+#define TYPE_DEF(nameOfClass)                                                         \
+const Type nameOfClass::type = std::hash<std::string>()(SYMBOL_TO_STR(nameOfClass));  \
+const char* nameOfClass::typeName = SYMBOL_TO_STR(nameOfClass);                       \
+//////////////////////////////////////////////
+
+//////////////////////////////////////////////
+// Use this version for base classes that DO have children.
+#define TYPE_DECL_BASE()                                            \
 private:                                                            \
     static const Type type;                                         \
                                                                     \
@@ -31,12 +48,12 @@ public:                                                             \
     static Type GetType() { return type; }                          \
     virtual bool IsTypeOf(const Type t) const { return t == type; } \
 
-// Add in base class definition (bottom of header or impl file).
-#define TYPE_DEF_BASE(nameOfClass)                                                  \
+#define TYPE_DEF_BASE(nameOfClass)                                                    \
 const Type nameOfClass::type = std::hash<std::string>()(SYMBOL_TO_STR(nameOfClass));  \
+//////////////////////////////////////////////
 
-// Adds type data to a child class. The base class must have declared TYPE_DECL_BASE.
-// Include this macro in the class declaration/header.
+//////////////////////////////////////////////
+// Use this version for child classes.
 #define TYPE_DECL_CHILD()                                   \
 private:                                                    \
     static const Type type;                                 \
@@ -45,11 +62,10 @@ public:                                                     \
     static Type GetType() { return type; }                  \
     virtual bool IsTypeOf(const Type t) const override;     \
 
-// Include this macro in the class definition (bottom of header or impl file).
 #define TYPE_DEF_CHILD(nameOfParentClass, nameOfChildClass)                                         \
-const Type nameOfChildClass::type = std::hash<std::string>()(SYMBOL_TO_STR(nameOfChildClass));        \
+const Type nameOfChildClass::type = std::hash<std::string>()(SYMBOL_TO_STR(nameOfChildClass));      \
                                                                                                     \
 bool nameOfChildClass::IsTypeOf(const Type t) const {                                               \
     return t == nameOfChildClass::type ? true : nameOfParentClass::IsTypeOf(t);                     \
 }                                                                                                   \
-
+//////////////////////////////////////////////
