f7330ae8cb7a785e3da879ac9920af88ff886b04
Clark Kromenaker <kromenak@gmail.com>
2023-09-08 09:54:38 -0700

    Added logic to skip movies triggered from Sheep code if the game is currently doing an action skip.
    This mimics the behavior of the original game (action skip before movies at Devil's Armchair for example). But also without this, the engine seems to get stuck in a deadlock!

--- Source/Sheep/API/SheepAPI_Rendering.cpp
+++ Source/Sheep/API/SheepAPI_Rendering.cpp
@@ -1,26 +1,38 @@
 #include "SheepAPI_Rendering.h"
 
+#include "ActionManager.h"
 #include "VideoPlayer.h"
 
 using namespace std;
 
 shpvoid PlayFullScreenMovie(const std::string& movieName)
 {
-    gVideoPlayer.Play(movieName, true, true, AddWait());
+    // Don't start a video during action skip. The game doesn't handle this well.
+    //TODO: Double check how original game handles this.
+    if(!gActionManager.IsSkippingCurrentAction())
+    {
+        gVideoPlayer.Play(movieName, true, true, AddWait());
+    }
     return 0;
 }
 RegFunc1(PlayFullScreenMovie, void, string, WAITABLE, REL_FUNC);
 
 shpvoid PlayFullScreenMovieX(const std::string& movieName, int autoclose)
 {
-    gVideoPlayer.Play(movieName, true, autoclose != 0 ? true : false, AddWait());
+    if(!gActionManager.IsSkippingCurrentAction())
+    {
+        gVideoPlayer.Play(movieName, true, autoclose != 0 ? true : false, AddWait());
+    }
     return 0;
 }
 RegFunc2(PlayFullScreenMovieX, void, string, int, WAITABLE, REL_FUNC);
 
 shpvoid PlayMovie(const std::string& movieName)
 {
-    gVideoPlayer.Play(movieName, false, true, AddWait());
+    if(!gActionManager.IsSkippingCurrentAction())
+    {
+        gVideoPlayer.Play(movieName, false, true, AddWait());
+    }
     return 0;
 }
 RegFunc1(PlayMovie, void, string, WAITABLE, REL_FUNC);
