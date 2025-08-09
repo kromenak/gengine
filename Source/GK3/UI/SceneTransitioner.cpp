2845d66f9e69f5f7a97202cc21f453360da412f2
Clark Kromenaker <kromenak@gmail.com>
2025-08-09 09:51:44 -1000

    Fixed Scene Transitioner being broken after recent changes.

--- Source/GK3/UI/SceneTransitioner.cpp
+++ Source/GK3/UI/SceneTransitioner.cpp
@@ -1,27 +1,11 @@
 #include "SceneTransitioner.h"
 
-#include "Texture.h"
-#include "UICanvas.h"
-#include "UIImage.h"
+#include "UIUtil.h"
 
 SceneTransitioner::SceneTransitioner() : Actor(TransformType::RectTransform)
 {
     // Draw scene transitioner above the normal scene.
-    AddComponent<UICanvas>(12);
-
-    // Canvas takes up entire screen.
-    RectTransform* rectTransform = GetComponent<RectTransform>();
-    rectTransform->SetSizeDelta(0.0f, 0.0f);
-    rectTransform->SetAnchorMin(Vector2::Zero);
-    rectTransform->SetAnchorMax(Vector2::One);
-
-    // Add overlay image that eats input.
-    mOverlay = AddComponent<UIImage>();
-    mOverlay->SetReceivesInput(true);
-
-    // Overlay is see-through, but it tints the screen.
-    mOverlay->SetTexture(&Texture::Black);
-    mOverlay->SetColor(Color32(0, 0, 0, 128));
+    UI::AddCanvas(this, 12, Color32(0, 0, 0, 128));
 }
 
 void SceneTransitioner::Show()
